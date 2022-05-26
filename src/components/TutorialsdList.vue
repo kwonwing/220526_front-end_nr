54813d356c9f5ae0d7d10bd883e300b82d11d87d
kwonwing <wing1559@naver.com>
2022-05-26 21:08:53 +0900

    220526_vue

---
+++ src/components/TutorialsdList.vue
@@ -0,0 +1,18 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    .list{
+        text-align: left;
+        max-width: 750px;
+        margin: auto;
+        
+    }
+</style>
